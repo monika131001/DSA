587cac2868c3d38b0538f697200abad223f448b9
MonikaAvhad <166313887+monika131001@users.noreply.github.com>
2026-08-04 17:11:25 +0530

    Solve Palindrome number

---
+++ Math/palindrome-number.js
@@ -0,0 +1,20 @@
+//Problem: Palindrome number
+//Pattern: String / Two pointers
+//Time: O(n)
+//Space: O(n)
+//Key idea: Convert the number to a string, then use two pointers from both ends to compare characters.
+
+var isPalindrome = function(x) {
+     if (x < 0) return false;
+
+    let s = String(x);
+    let i = 0;
+    let j = Math.floor(s.length-1);
+
+    while(i<j) {
+        if(s[i] !== s[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+};
